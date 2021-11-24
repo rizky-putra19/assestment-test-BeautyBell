e7b417b3ca9fb7573eee9614493560e04f7db4aa
rizky-putra19 <rizkyagrasyahputra19@gmail.com>
2021-11-25 04:12:41 +0700

    add file for deployment

--- config/config.js
+++ config/config.js
@@ -9,17 +9,21 @@ module.exports =
     "dialect": "postgres"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": "test_warehouse",
     "host": "127.0.0.1",
-    "dialect": "mysql"
+    "dialect": "postgres"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "use_env_variable": "DATABASE_URL",
+    "protocol": "postgres",
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "rejectUnauthorized": false
+      }
+    }
   }
 }
+
